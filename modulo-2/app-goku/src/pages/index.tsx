feb9c1c272f7749999bb994d79b20b1d8152c212
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-06 19:46:32 -0300

    refactor: Improve UI layout and styles in home page

--- modulo-2/app-goku/src/pages/index.tsx
+++ modulo-2/app-goku/src/pages/index.tsx
@@ -11,20 +11,21 @@ interface HomeProps {
 export default function Home({ characters }: HomeProps) {
 	return (
 		<Layout title="App de Goku" description="App de Goku con Next.js">
-			<h1>App de Goku</h1>
+			<h1 className="mb-6 text-3xl">App de Goku</h1>
 			<div className="grid grid-cols-3 gap-8">
 				{characters.items.map((character) => (
-					<Link className="border rounded-md" key={character.id} href={`/character/${character.name}`}>
+					<Link className="border rounded-md hover:scale-105 transition-transform duration-500" key={character.id} href={`/character/${character.name}`}>
 						<div
-							className="flex flex-col items-center justify-center p-6"
+							className="flex flex-col gap-4 items-start justify-center p-6"
 						>
 							<Image
+								className="w-40 h-40 object-contain"
 								width={160}
 								height={160}
 								src={character.image}
 								alt={character.name}
 							/>
-							<p>{character.name}</p>
+							<p className="text-base font-semibold">{character.name}</p>
 						</div>
 					</Link>
 				))}
