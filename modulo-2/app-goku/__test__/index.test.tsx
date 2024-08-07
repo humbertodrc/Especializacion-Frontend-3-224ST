60486d8ca4b51b15787930caaf67ba15c199ef4d
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-06 22:29:48 -0300

    Add getServerSideProps in Home page

--- modulo-2/app-goku/__test__/index.test.tsx
+++ modulo-2/app-goku/__test__/index.test.tsx
@@ -1,61 +1,56 @@
 import {expect, test, describe} from "vitest";
 import {render, screen, waitFor} from "@testing-library/react";
 import Home from "@/pages";
-import {Characters } from "@/interface/characters";
+import {Characters, Item, Links, Meta } from "@/interface/characters";
 
-const characters: Characters = {
-	items: [
-		{
-			id: 11,
-			name: "Krillin",
-			ki: "1.000.000",
-			maxKi: "1 Billion",
-			race: "Human",
-			gender: "Male",
-			description:
-				"Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z y Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En el Arco de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron.",
-			image: "https://dragonball-api.com/characters/Krilin_Universo7.webp",
-			affiliation: "Z Fighter",
-			deletedAt: null,
-		},
-		{
-			id: 12,
-			name: "Tenshinhan",
-			ki: "2.400.000",
-			maxKi: "80.000.000",
-			race: "Human",
-			gender: "Male",
-			description:
-				"Maestro de las artes marciales y miembro de los Z Fighters.  Es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super.",
-			image: "https://dragonball-api.com/characters/Tenshinhan_Universo7.webp",
-			affiliation: "Z Fighter",
-			deletedAt: null,
-		},
-	],
-	meta: {
-		totalItems: 58,
-		itemCount: 10,
-		itemsPerPage: 10,
-		totalPages: 6,
-		currentPage: 2,
-	},
-	links: {
-		first: "https://dragonball-api.com/api/characters?limit=10",
-		previous: "https://dragonball-api.com/api/characters?page=1&limit=10",
-		next: "https://dragonball-api.com/api/characters?page=3&limit=10",
-		last: "https://dragonball-api.com/api/characters?page=6&limit=10",
-	},
+const characters: Item[] = [
+  {
+    "id": 1,
+    "name": "Goku",
+    "ki": "60.000.000",
+    "maxKi": "90 Septillion",
+    "race": "Saiyan",
+    "description": "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador.",
+    "image": "https://dragonball-api.com/characters/goku_normal.webp",
+    "deletedAt": null
+},
+{
+    "id": 2,
+    "name": "Vegeta",
+    "ki": "54.000.000",
+    "maxKi": "19.84 Septillion",
+    "race": "Saiyan",
+    "description": "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie.",
+    "image": "https://dragonball-api.com/characters/vegeta_normal.webp",
+    "deletedAt": null
+}
+];
+
+const meta: Meta = {
+  totalItems: 2,
+  itemCount: 2,
+  itemsPerPage: 2,
+  totalPages: 1,
+  currentPage: 1,
 };
+
+const links: Links = {
+  first: '',
+  previous: '',
+  next: '',
+  last: '',
+};
+
 
 describe("Test in Home", () => {
 	test("Should title app", () => {
-		render(<Home characters={characters} />);
+		render(<Home characters={characters} meta={meta} links={links} />);
 		const title = screen.getByText("App de Goku");
 		expect(title).toBeTruthy();
 	});
 
 	test("Should Characters", async () => {
-    render(<Home characters={characters} />);
+    render(<Home characters={characters} meta={meta} links={links} />);
     screen.debug();
 		waitFor(() => {
 			const krillin = screen.getByText("Krillin");
--- modulo-2/app-goku/src/pages/index.tsx
+++ modulo-2/app-goku/src/pages/index.tsx
@@ -1,19 +1,21 @@
 import Layout from "@/components/layouts/Layout";
-import {Characters} from "@/interface/characters";
+import {Item, Links, Meta} from "@/interface/characters";
 import {GetServerSideProps} from "next";
 import Image from "next/image";
 import Link from "next/link";
 
 interface HomeProps {
-	characters: Characters;
+	characters: Item[];
+	meta: Meta;
+	links: Links;
 }
 
-export default function Home({ characters }: HomeProps) {
+export default function Home({ characters, meta, links }: HomeProps) {
 	return (
 		<Layout title="App de Goku" description="App de Goku con Next.js">
 			<h1 className="mb-6 text-3xl">App de Goku</h1>
 			<div className="grid grid-cols-3 gap-8">
-				{characters.items.map((character) => (
+				{characters.map((character) => (
 					<Link className="border rounded-md hover:scale-105 transition-transform duration-500" key={character.id} href={`/character/${character.name}`}>
 						<div
 							className="flex flex-col gap-4 items-start justify-center p-6"
@@ -30,6 +32,18 @@ export default function Home({ characters }: HomeProps) {
 					</Link>
 				))}
 			</div>
+			<div className="flex gap-4 mt-4">
+				{links.previous && (
+					<Link className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" href={`/?page=${meta.currentPage - 1}`}>
+						Previus
+					</Link>
+				)}
+				{links.next && (
+					<Link className="flex items-center justify-center px-3 h-8 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" href={`/?page=${meta.currentPage + 1}`}>
+						Next
+					</Link>
+				)}
+			</div>
 		</Layout>
 	);
 }
@@ -38,12 +52,18 @@ export default function Home({ characters }: HomeProps) {
 // - Only if you need to pre-render a page whose data must be fetched at request time
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const res = await fetch(`https://dragonball-api.com/api/characters?limit=12`);
+	const page = ctx.query.page ? ctx.query.page : 1;
+	console.log(page);
+	const res = await fetch(`https://dragonball-api.com/api/characters?page=${page}&limit=12`);
 	const data = await res.json();
+
+	ctx.res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate=59");
 
 	return {
 		props: {
-			characters: data,
+			characters: data.items,
+			meta: data.meta,
+			links: data.links,
 		},
 	};
 };
